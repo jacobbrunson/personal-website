100ded33d7c23725d1d12401df644d6a0586ff00
Jacob Brunson <jacob@brunson.me>
2021-07-04 13:55:13 -0700

    Resolves #4 Add GitHub CTA. Also a few other minor improvements

--- brunson-app/app.js
+++ brunson-app/app.js
@@ -18,7 +18,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.disable('x-powered-by');
+app.disable("x-powered-by");
 
 // Template routes
 const routes = [];
